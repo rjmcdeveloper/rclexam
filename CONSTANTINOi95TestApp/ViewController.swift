99dd452f0eac21e94afd297f66af68bc823ac42c
joriel <jorielfronda@rcclapac.com>
2017-11-20 00:22:30 +0800

    Initial Commit

---
+++ CONSTANTINOi95TestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CONSTANTINOi95TestApp
+//
+//  Created by Joriel Oller Fronda on 20/11/2017.
+//  Copyright © 2017 Rj Constantino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
